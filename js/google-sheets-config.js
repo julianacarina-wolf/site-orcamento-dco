24dee2c08fe55f35b94f5a3410acad5d4b4676ce
julianacarina-wolf <juliana.carina@gmail.com>
2026-07-30 17:36:21 -0300

    Update Google Sheets API URL in config

--- js/google-sheets-config.js
+++ js/google-sheets-config.js
@@ -1,6 +1,6 @@
 window.GOOGLE_SHEETS_CONFIG = {
   // URL do aplicativo da Web publicado no Google Apps Script.
-  apiUrl: 'https://script.google.com/macros/s/AKfycbwt_fgHT2k5BigWuUG4JxOqfnoqRLgsno4CLpCDcaeaef2z6FuwBWPKv19tgM1O7hZS9A/exec',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbwyYCYxkG-jGeWBra0OcG18sfTBfgcr_V-GyXXmpY_MpLScFxQNB2cED7UZzxBEAkfXzQ/exec',
 
   // Opcional. Preencha somente se a propriedade API_KEY também tiver sido criada no Apps Script.
   apiKey: '',
